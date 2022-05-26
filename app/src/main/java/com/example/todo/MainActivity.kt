9444d82370589892f29f955e821d2e00c87a8514
Chirag <chiraggoyal1910@gmail.com>
2022-05-26 16:48:17 +0530

    add fragment layout complete

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.todo
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageButton
+import android.widget.EditText
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
-    var dialog: BottomSheetDialog? = null;
+    private var dialog: BottomSheetDialog? = null;
+    private var titleField: EditText? = null;
+    private var descField: EditText? = null;
+    private var dateField: EditText? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,16 +21,11 @@ class MainActivity : AppCompatActivity() {
     fun addButtonAction(view: View) {
         dialog = BottomSheetDialog(view.context)
         dialog!!.setContentView(layoutInflater.inflate(R.layout.fragment_add, null))
+        dialog!!.behavior.state = BottomSheetBehavior.STATE_EXPANDED;
         dialog!!.show()
     }
 
-    // wht to do fcking shit
-    //zip this folder and send to me
-    // mehh. will just upload to github..wait
-
-
     fun hideDialog(view: View) {
-        Log.d("error", "Why the fck not updating")
         dialog!!.hide()
     }
 }
